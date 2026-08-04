c86ff83bc351fa5c2c57ca7dee5e6770ac09f86c
dkochergin <dmytro.kochergin@dxc.com>
2026-08-04 16:14:45 +0300

    fireball is started from the center of player, start it a bit to the direction of casting

--- game/src/systems/WeaponSystem.ts
+++ game/src/systems/WeaponSystem.ts
@@ -134,7 +134,18 @@ export class WeaponSystem {
 
     this.audioSystem?.playFireballCast(0.45);
 
-    const projectile = this.scene.physics.add.sprite(this.player.x, this.player.y, "fireball");
+    const castVector = new Phaser.Math.Vector2(targetX - this.player.x, targetY - this.player.y);
+    const spawnOffset = Math.min(20, castVector.length() * 0.5);
+
+    if (spawnOffset > 0) {
+      castVector.normalize().scale(spawnOffset);
+    }
+
+    const projectile = this.scene.physics.add.sprite(
+      this.player.x + castVector.x,
+      this.player.y + castVector.y,
+      "fireball"
+    );
     projectile.setPipeline("Light2D");
     projectile.setDepth(250);
 
@@ -253,7 +264,18 @@ export class WeaponSystem {
 
     this.audioSystem?.playFireballCast(kind === "ray" ? 0.35 : 0.55);
 
-    const projectile = this.scene.physics.add.sprite(this.player.x, this.player.y, textureKey);
+    const castVector = new Phaser.Math.Vector2(targetX - this.player.x, targetY - this.player.y);
+    const spawnOffset = Math.min(20, castVector.length() * 0.5);
+
+    if (spawnOffset > 0) {
+      castVector.normalize().scale(spawnOffset);
+    }
+
+    const projectile = this.scene.physics.add.sprite(
+      this.player.x + castVector.x,
+      this.player.y + castVector.y,
+      textureKey
+    );
     projectile.setPipeline("Light2D");
     projectile.setDepth(kind === "ray" ? 255 : 253);
     projectile.setCollideWorldBounds(false);
